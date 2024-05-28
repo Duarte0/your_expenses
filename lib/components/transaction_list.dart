a17bfdf5d0a849b8c9d0f4dc20b7dc9f1023b4cc
Duarte <guiduart3223@gmail.com>
2024-05-28 14:21:23 -0300

    refatorado usando ListTitle

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -13,51 +13,61 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 320,
-        child: ListView.builder( 
-          itemCount: transaction.length,
-          itemBuilder: (context, index) {
-            final tr = transaction[index];
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.deepPurple,
-                        width: 2,
-                      )),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.deepPurple,
-                        ),
-                      )),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tr.title,
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
+        height: 355,
+        child: transaction.isEmpty
+            ? Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text(
+                    'Nenhuma Transação Cadastrada!',
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
                   )
                 ],
-              ),
-            );
-          },
-        ));
+              )
+            : ListView.builder(
+                itemCount: transaction.length,
+                itemBuilder: (context, index) {
+                  final tr = transaction[index];
+                  return Card(
+                    elevation: 5,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 5, 
+                      vertical: 8
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6),
+                          child: FittedBox(
+                            child: Text(
+                              'R\$${tr.value}',
+                              style: const TextStyle(
+                                color: Colors.white
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        tr.title,
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      subtitle: Text(
+                        DateFormat('d MMM y').format(tr.date)
+                      ),
+                    ),
+                  );
+                },
+              ));
   }
 }
